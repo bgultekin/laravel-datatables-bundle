eb5663cad6399f8e77fa73a455c64ee5a0308f6c
Bilal <bilal@bilal.im>
2012-11-09 01:14:17 +0200

    deleting trash line
    #4 deleting trash line

--- Datatables.php
+++ Datatables.php
@@ -350,8 +350,6 @@ class Datatables
 						$column = explode(' as ',$copy_this->columns[$i]);
 						$column = array_shift($column);
 						$query->or_where($column,'LIKE','%'.Input::get('sSearch').'%');
-
-						$first_time = false;
 					}
 				}
 			});
